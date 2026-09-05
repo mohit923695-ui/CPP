d26c321dbac5d4b90355b0f7fab205ce04325312
mohit923695-ui <mohit923695@gmail.com>
2026-09-05 16:36:29 +0530

    comments

--- Chapter_01/03_comments.cpp
+++ Chapter_01/03_comments.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a = 4;
+    int a = 4;       // build in data type   // single line comments 
     int b = 6;
     float pi = 3.14;
     std::cout << "the value of a is " << a << " : the value of b is " << b;
     cout << "the value of pi :" << pi;
     return 0;
 }
+
+/*
+the 
+program gone 
+*/
