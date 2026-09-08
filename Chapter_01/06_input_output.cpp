58190f77d3a7320f166741bd14c2616a0b2c904d
mohit923695-ui <mohit923695@gmail.com>
2026-09-08 11:58:47 +0530

    input output

---
+++ Chapter_01/06_input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 ,num2;
+    cout<<"enter the num1: ";
+    cin >> num1;
+
+    cout<<"enter the num2: ";
+    cin >> num2;
+
+    cout<<"the value is " << num1 + num2;
+    return 0;
+}
