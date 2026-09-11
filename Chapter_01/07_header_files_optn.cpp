0daa6d5be89d439d98a18488970858843a09499f
mohit923695-ui <mohit923695@gmail.com>
2026-09-11 12:01:11 +0530

    header files oprater

---
+++ Chapter_01/07_header_files_optn.cpp
@@ -0,0 +1,11 @@
+#include<iostream>  //header file
+// system header files = it comes with the compilier  
+//user defined header files = it is written by the programmer
+
+using namespace std;
+
+int main(){
+    cout<< "hello world ";
+    return 0;
+
+}
