a4b0810667221e15ef94ed5b29e8092892bf1fbc
mohit923695-ui <mohit923695@gmail.com>
2026-09-11 12:30:13 +0530

    assigmnet operator

---
+++ Chapter_01/09_assigment_operartors.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4 , b =  5;
+    char c = 'r';
+     // assigment operator
+    cout<<"the value is "<<(a+b);
+    return 0 ;
+}
