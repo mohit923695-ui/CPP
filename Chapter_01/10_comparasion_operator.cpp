168449b98cefd0ee159cf9cd117b9a75c3def8a3
mohit923695-ui <mohit923695@gmail.com>
2026-09-11 12:40:45 +0530

    comparetion operater

---
+++ Chapter_01/10_comparasion_operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>  //header file
+// system header files = it comes with the compilier  
+//user defined header files = it is written by the programmer
+
+using namespace std;
+
+int main(){
+    int a = 3 ,b = 34;
+    char c = 'f';
+    cout<< "hello world ";
+    cout<< "the value is a+f =  "<<(a+b);
+
+    return 0;
+
+}
