b501848529a8e7c0a6cca1671b21834075c53c14
mohit923695-ui <mohit923695@gmail.com>
2026-09-11 12:17:58 +0530

    arthmatic operations

---
+++ Chapter_01/08_arthmatic_operation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4 , b =  5;
+     cout <<"hello mohit";
+
+     // arthmatic operation
+    cout<<"the value is a + b= "<<a+b<<endl;
+    cout<<"the value is a - b= "<<a-b<<endl;
+    cout<<"the value is a * b= "<<a*b<<endl;
+    cout<<"the value is a / b= "<<a/b<<endl;
+    cout<<"the value is a % b= "<<a%b<<endl;
+    cout<<"the value is a a--b=  "<<a--<<endl;
+    cout<<"the value is ++a b= "<<++a<<endl;
+    cout<<"the value is --a b= "<<--a<<endl;
+    return 0 ;
+}
