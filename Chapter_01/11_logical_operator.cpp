ca07fb870b5e94b63973ca8d187766584ff7554a
mohit923695-ui <mohit923695@gmail.com>
2026-09-11 12:41:07 +0530

    logical operator

---
+++ Chapter_01/11_logical_operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4 , b =  5;
+     // logical oprator
+     cout<<"the value is : (a==b) && (a<b)= "<<((a==b) && (a<b))<<endl;
+     cout<<"the value is : (a==b) || (a<b)= "<<((a==b) || (a<b))<<endl;
+     cout<<"the value is : !(a==b)=  "<<!(a==b)<<endl;
+
+    return 0 ;
+}
